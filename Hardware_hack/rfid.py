1f34fe2979829c3e1af5c3740397f9da1039935d
aravindA11 <44811786+aravindA11@users.noreply.github.com>
2019-03-12 00:42:15 +0530

    Update rfid.py

--- Hardware_hack/rfid.py
+++ Hardware_hack/rfid.py
@@ -1,7 +1,9 @@
-import serial                                                  #import serial module
-
-   ser = serial.Serial ("/dev/ttyAMA0")                           #Open named port 
-   ser.baudrate = 9600                                            #Set baud rate to 9600
-   data = ser.read(12)
-   print "hello"                                            #Read 12 characters from serial port to data
-  # ser.close ()                                                   #Close port             
+import RPi.GPIO as GPIO
+import SimpleMFRC522
+reader = SimpleMFRC522.SimpleMFRC522()
+try:
+        id, text = reader.read()
+        print(id)
+        print(text)
+finally:
+        GPIO.cleanup()
